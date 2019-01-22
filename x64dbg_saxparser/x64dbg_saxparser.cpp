33486831bf1097b475a275ac78874e71a347a04b
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2019-01-22 22:18:10 +0100

    more or less working strategy

--- x64dbg_saxparser/x64dbg_saxparser.cpp
+++ x64dbg_saxparser/x64dbg_saxparser.cpp
@@ -62,93 +62,55 @@ static string serialize(const Document& doc)
     return buffer.GetString();
 }
 
-struct EmptyHandler : public BaseReaderHandler<UTF8<>>
-{
-    std::function<void(const GenericDocument<UTF8<>>&)> cb;
-};
-
 struct ArrayElementCollector
 {
     virtual bool CollectElement(Document& document) = 0;
 };
 
-class ArrayHandler : public BaseReaderHandler<UTF8<>, ArrayHandler>
-{
-    int objectNesting = 0;
-    Document document;
-    ArrayElementCollector* collector = nullptr;
-
-public:
-    ArrayHandler() { }
-    ArrayHandler(ArrayElementCollector* collector) : collector(collector) { }
-
-    bool Null() { return document.Null(); }
-    bool Bool(bool b) { return document.Bool(b); }
-    bool Int(int i) { return document.Int(i); }
-    bool Uint(unsigned u) { return document.Uint(u); }
-    bool Int64(int64_t i) { return document.Int64(i); }
-    bool Uint64(uint64_t u) { return document.Uint64(u); }
-    bool Double(double d) { return document.Double(d); }
-    bool String(const char* str, SizeType length, bool copy) { return document.String(str, length, copy); }
-    bool Key(const char* str, SizeType length, bool copy) { return document.Key(str, length, copy); }
-    bool StartArray() { return document.StartArray(); }
-    bool EndArray(SizeType elementCount) { return document.EndArray(elementCount); }
-
-    bool StartObject()
-    {
-        objectNesting++;
-        return document.StartObject();
-    }
-
-    bool EndObject(SizeType memberCount)
-    {
-        objectNesting--;
-        if(document.EndObject(memberCount))
-        {
-            if(objectNesting == 0)
-            {
-                auto g = [](Document&) { return true; };
-                document.Populate(g);
-                if(!collector->CollectElement(document))
-                    return false;
-                document.SetNull();
-            }
-            return true;
-        }
-        return false;
-    }
-};
-
+/// <summary>
+/// The purpose of this structure is to handle SAX events for JSON documents in the following form:
+/// {
+///   "array1": [],
+///   "array2": [],
+///   "other": {},
+///   "data": 1
+/// }
+/// The user can specify ArrayElementCollector instances to collect array elements in a streaming fashion
+/// (eg not building up a DOM in memory), but keys that do not have handlers will combine to a DOM.
+/// This is meant for JSON files where the arrays with handlers contain many elements (taking memory),
+/// but there the rest of the document is simple configuration.
+/// </summary>
 class SaxHandler : public BaseReaderHandler<UTF8<>, SaxHandler>
 {
-    std::map<std::string, ArrayHandler> handlers;
-    ArrayHandler* h = nullptr;
+    std::map<std::string, ArrayElementCollector*> handlers;
+    ArrayElementCollector* collector = nullptr;
     bool inArray = false;
     int objectNesting = 0;
     int arrayNesting = 0;
+    Document element;
 
 public:
-    bool Null() { return inArray ? h->Null() : true; }
-    bool Bool(bool b) { return inArray ? h->Bool(b) : true; }
-    bool Int(int i) { return inArray ? h->Int(i) : true; }
-    bool Uint(unsigned u) { return inArray ? h->Uint(u) : true; }
-    bool Int64(int64_t i) { return inArray ? h->Int64(i) : true; }
-    bool Uint64(uint64_t u) { return inArray ? h->Uint64(u) : true; }
-    bool Double(double d) { return inArray ? h->Double(d) : true; }
-    bool String(const char* str, SizeType length, bool copy) { return inArray ? h->String(str, length, copy) : true; }
+    bool Null() { return inArray ? element.Null() : true; }
+    bool Bool(bool b) { return inArray ? element.Bool(b) : true; }
+    bool Int(int i) { return inArray ? element.Int(i) : true; }
+    bool Uint(unsigned u) { return inArray ? element.Uint(u) : true; }
+    bool Int64(int64_t i) { return inArray ? element.Int64(i) : true; }
+    bool Uint64(uint64_t u) { return inArray ? element.Uint64(u) : true; }
+    bool Double(double d) { return inArray ? element.Double(d) : true; }
+    bool String(const char* str, SizeType length, bool copy) { return inArray ? element.String(str, length, copy) : true; }
 
     bool Key(const char* str, SizeType length, bool copy)
     {
         if(inArray)
-            return h->Key(str, length, copy);
+            return element.Key(str, length, copy);
         if(objectNesting == 1 && arrayNesting == 0)
         {
             string key(str, length);
             auto itr = handlers.find(key);
             if(itr != handlers.end())
-                h = &itr->second;
+                collector = itr->second;
             else
-                h = nullptr;
+                collector = nullptr;
         }
         return true;
     }
@@ -156,21 +118,37 @@ public:
     bool StartObject()
     {
         objectNesting++;
-        return inArray ? h->StartObject() : true;
+        return inArray ? element.StartObject() : true;
     }
 
     bool EndObject(SizeType memberCount)
     {
         objectNesting--;
-        return inArray ? h->EndObject(memberCount) : true;
+        if(inArray)
+        {
+            if(element.EndObject(memberCount))
+            {
+                if(objectNesting == 1)
+                {
+                    auto g = [](Document&) { return true; };
+                    element.Populate(g);
+                    if(!collector->CollectElement(element))
+                        return false;
+                    element.SetNull();
+                }
+                return true;
+            }
+            return false;
+        }
+        return true;
     }
 
     bool StartArray()
     {
         arrayNesting++;
         if(inArray)
-            return h->StartArray();
-        if(objectNesting == 1 && arrayNesting == 1 && h != nullptr)
+            return element.StartArray();
+        if(objectNesting == 1 && arrayNesting == 1 && collector != nullptr)
             inArray = true;
         return true;
     }
@@ -180,12 +158,12 @@ public:
         if(arrayNesting == 1 && objectNesting == 1)
             inArray = false;
         arrayNesting--;
-        return inArray ? h->EndArray(elementCount) : true;
+        return inArray ? element.EndArray(elementCount) : true;
     }
 
     void SetArrayCollector(const std::string& key, ArrayElementCollector* collector)
     {
-        handlers[key] = ArrayHandler(collector);
+        handlers[key] = collector;
     }
 };
 
